1abe4a4e4f553e2c6c3a749224db336e2d47ec12
prann34 <prann3928@gmail.com>
2025-08-03 14:18:45 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,13 @@
+a=input("Enter the first number: ")
+a=int(a)
+b=input("Enter the second number: ")
+b=int(b)
+print("\n")
+#addition
+print("Addition: ",a+b)
+#Substraction
+print("Substraction: ",a-b)
+#€Multiplication
+print("Multiplication: ",a*b)
+#Division
+print("Division: ",a/b)
